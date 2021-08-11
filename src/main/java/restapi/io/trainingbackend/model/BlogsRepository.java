1ef4791e642452969d3a697b48f5fdaedbaf9ead
Tra Thao Nguyen <17520831@gm.uit.edu.vn>
2021-08-11 23:36:52 +0700

    gdfg

--- src/main/java/restapi/io/trainingbackend/model/BlogsRepository.java
+++
@@ -1,8 +0,0 @@
-package restapi.io.trainingbackend.model;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import restapi.io.trainingbackend.entity.Blogs;
-
-public interface BlogsRepository extends JpaRepository<Blogs, Integer> {
-
-}
