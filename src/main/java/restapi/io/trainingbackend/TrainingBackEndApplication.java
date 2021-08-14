b8b0f17f2b742df75052e245448c636d1ed3b433
Tra Thao Nguyen <17520831@gm.uit.edu.vn>
2021-08-14 20:20:31 +0700

    gfgfd

--- src/main/java/restapi/io/trainingbackend/TrainingBackEndApplication.java
+++ src/main/java/restapi/io/trainingbackend/TrainingBackEndApplication.java
@@ -1,10 +1,10 @@
 package restapi.io.trainingbackend;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class TrainingBackEndApplication  {
@@ -13,5 +13,13 @@ public class TrainingBackEndApplication  {
 		SpringApplication.run(TrainingBackEndApplication.class, args);
 	}
 
-
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("http://localhost:3000");
+			}
+		};
+	}
 }
